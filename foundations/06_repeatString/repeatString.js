b19418fb66f649e8b68faf9bc61bfbdb460e6473
Scarce85 <58633373+Scarce85@users.noreply.github.com>
2026-05-25 18:01:27 +0300

    Finished Arrays & Loops Assignment Exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let result = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    else if (typeof num === 'number' && !isNaN(num)) {
+        for (let i = 0; i < num; i++) {
+            result += string;
+        }
+        return result;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
 
+    else if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+
+    return ((max - min) + 1) * (min + max) / 2;
 };
 
 // Do not edit below this line
